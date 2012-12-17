7a2d47f9de0adc6846cfc53b7d2e85a378fbe34d
giacomocamerano <giacomo.camerano@calleidos.it>
2012-12-17 12:38:46 +0100

    Correzione errore in image/document type controllers

--- web/Controller/DocumenttypesController.php
+++ web/Controller/DocumenttypesController.php
@@ -8,7 +8,7 @@ App::uses('AppController', 'Controller');
 class DocumenttypesController extends AppController {
 	
 	public function admin_listDocumentTypes($posttype) {
-		$types=$this->Documenttype->DocumenttypesPosttype->find('list', array('conditions' => array('posttype_id' => $posttype), 'fields' => array('id')));
+		$types=$this->Documenttype->DocumenttypesPosttype->find('list', array('conditions' => array('posttype_id' => $posttype), 'fields' => array('documenttype_id')));
 		$types=$this->Documenttype->find('list', array('conditions' => array('id' => $types)));
 		return $types;
 	}
